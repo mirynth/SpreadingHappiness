67db1667cde34eb3f4271e3356b0492330486799
sykuub <157003871+sykuub@users.noreply.github.com>
2024-01-18 22:42:59 +1030

    added homing missile move pattern

--- Assets/Scripts/MagicalGirls/MagicalGrilsStates/AngryStatePlaceholder.cs
+++ Assets/Scripts/MagicalGirls/MagicalGrilsStates/AngryStatePlaceholder.cs
@@ -4,31 +4,77 @@ using UnityEngine;
 
 public class AngryStatePlaceholder : AbstractAngryState
 {
-	float wave_scale = 1.0f;
+	float shoot_up = 1.0f;
+	int barrage_count = 0;
 
-	public override float CooldownTimeBeforeShooting {get {return 0.1f;}}
+	public override float CooldownTimeBeforeShooting {get {return 2.05f;}}
 
 	public AngryStatePlaceholder(AbstractMagicalGirlController mg) : base(mg)
 	{}
 
-	void ShootWave()
+	void ShootWaveExample()
 	{
-		// This shoot function should be a lot more sophisticated
+		// The shoot function should be a lot more sophisticated
 		// but as an example we will shoot boba in a wave
 
+		// fix to create a barrage, will need to create a way to specify attack patterns.
+		this.barrage_count += 1;
+		if (this.barrage_count > 30) { return; }
+
+		// Create the projectile
 		GameObject projectileObject = GameObject.Instantiate(magicalGirl.enemyBulletPrefab, 
 			magicalGirl.rigid2d.position + Vector2.up * 0.5f, 
 			Quaternion.identity);
-
 		EnemyBulletController projectile = projectileObject.GetComponent<EnemyBulletController>();
-		projectile.SetPattern(new BobaPatternWave(Vector2.left, projectileObject.transform.position, 1.2f * this.wave_scale, 5.0f, 5.0f));
 
-		// fix to flip wave direction each time we shoot to complete the example.
-		this.wave_scale *= -1.0f;
+		// Create and set the move pattern
+		projectile.SetPattern(new BobaPatternWave(Vector2.left, projectileObject.transform.position, 4.0f * this.shoot_up, 4.0f, 20.0f));
+
+		// simple fix to flip wave direction each time we shoot.
+		this.shoot_up *= -1.0f;
 	}
 
-	public override void Shoot(){ ShootWave(); }
-	// public override void Shoot(){this.ShootInCircle(300, BulletType.Wrath);}
+	void ShootMissileExample()
+	{
+		// as another example we will shoot a missile boba
+
+		// fix to create a barrage, we will need to create a better way to specify attack bursts.
+		this.barrage_count += 1;
+		if (this.barrage_count > 6) { return; }
+
+		// Create projectile
+		GameObject projectileObject = GameObject.Instantiate(magicalGirl.enemyBulletPrefab, 
+			magicalGirl.rigid2d.position + Vector2.up * 0.5f, 
+			Quaternion.identity);
+        EnemyBulletController projectile = projectileObject.GetComponent<EnemyBulletController>();
+
+		// Get the main character (through a singleton, sorry!)
+		Rigidbody2D main_character_rigidbody = MainCharacterController.instance.GetComponent<Rigidbody2D>();
+        Vector2 dir_to_main_character = (main_character_rigidbody.transform.position - projectileObject.transform.position).normalized;
+
+		// launch dir will be ~70 degrees off from dir_to_main_character
+        Vector2 launch_dir = Vector2.MoveTowards(
+            dir_to_main_character,
+            Vector2.Perpendicular(dir_to_main_character) * this.shoot_up,
+            Mathf.Sin(70.0f)
+        ).normalized;
+
+		// Create and set the projectile move pattern
+        projectile.SetPattern(new BobaPatternLaunchedMissile(
+            main_character_rigidbody,
+            launch_dir,
+            8.0f,
+            1.0f,
+            20.0f,
+            1.0f
+        ));
+
+		// fix to flip direction each time we shoot.
+		this.shoot_up *= -1.0f;
+    }
+
+    //public override void Shoot(){ ShootMissileExample(); }
+	public override void Shoot(){this.ShootInCircle(300, BulletType.Wrath);}
 
 	public override void Move(){}
 }
---
+++ Assets/Scripts/Projectiles/BobaPatternLaunchedMissile.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobaPatternLaunchedMissile : AbstractBobaPattern
+{
+    enum MissileState
+    {
+        LaunchState,
+        HomingState,
+    }
+
+    Rigidbody2D target;
+    Vector2 velocity;
+    float homing_delay;
+    float homing_delay_current;
+    float homing_speed;
+    float max_turning_rate;
+    MissileState missile_state;
+
+    public BobaPatternLaunchedMissile(Rigidbody2D target, Vector2 launch_direction, float launch_speed, float homing_delay, float homing_speed, float max_turning_rate)
+    {
+        this.target = target;
+        this.velocity = launch_direction.normalized * launch_speed;
+        this.homing_delay = homing_delay;
+        this.homing_delay_current = homing_delay;
+        this.homing_speed = homing_speed;
+        this.max_turning_rate = max_turning_rate;
+        this.missile_state = MissileState.LaunchState;
+    }
+
+    public override void onFixedUpdate(Rigidbody2D boba_body)
+    {
+        switch (this.missile_state)
+        {
+            case MissileState.LaunchState:
+                // When in the launch state, move along velocity given in constructor and slow down towards the end. 
+                float t = (homing_delay_current / homing_delay);
+                boba_body.MovePosition((Vector2)boba_body.transform.position + (this.velocity * t * Time.fixedDeltaTime));
+
+                // countdown to the transition into homing state
+                homing_delay_current -= Time.fixedDeltaTime;
+                if (homing_delay_current < 0.0f)
+                {
+                    // When the timer finishes, set to homing state and set velocity towards player
+                    this.missile_state = MissileState.HomingState;
+                    this.velocity = (target.transform.position - boba_body.transform.position).normalized * homing_speed;
+                }
+                break;
+
+            case MissileState.HomingState:
+                // When in homing state we slightly adjust missile velocity towards
+                // player each frame limited by the max turning rate
+                Vector2 new_dir = Vector2.MoveTowards(this.velocity.normalized, (target.transform.position - boba_body.transform.position).normalized, Mathf.Sin(this.max_turning_rate * Time.fixedDeltaTime));
+                this.velocity = new_dir.normalized * this.velocity.magnitude;
+
+                boba_body.MovePosition((Vector2)boba_body.transform.position + this.velocity * Time.fixedDeltaTime);
+
+                // Destroy the bullet after another 4 seconds.
+                // if we dont do this the bullet eventually comes back...
+                homing_delay_current -= Time.fixedDeltaTime;
+                if (homing_delay_current < -4.0f)
+                {
+                    UnityEngine.MonoBehaviour.Destroy(boba_body.gameObject);
+                }
+                break;
+        }
+    }
+}
